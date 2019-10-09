cb9d32e43788345edc0833822135434dcb281034
Wade C Roundy <46587419+wcroundy@users.noreply.github.com>
2019-10-08 20:50:46 -0600

    Create 101.js

---
+++ functions/101.js
@@ -0,0 +1,15 @@
+let funcname = function()
+{
+    console.log("This is a function")
+}
+funcname()
+funcname()
+let input = 10
+let square = function(input)
+{
+    let output = input * input
+    return output
+}
+
+let ans = square(input)
+console.log(ans)
