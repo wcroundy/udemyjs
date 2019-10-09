53e65df11c8f0931a67588fc678d00c1fce2dcae
Wade C Roundy <46587419+wcroundy@users.noreply.github.com>
2019-10-08 20:50:28 -0600

    Create logic.js

---
+++ basics/logic.js
@@ -0,0 +1,13 @@
+let temp = 72
+//if(temp >= 60)
+//{
+//    if(temp <= 90)
+//    {
+//        console.log("its pretty nice out")
+//    }
+//}
+
+if(temp <= 90 && temp >= 60)
+{
+    console.log("its pretty nice out")
+}
