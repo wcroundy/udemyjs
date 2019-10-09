895e996cd13ac30b84c8e38b2a68072b3b230a6e
Wade C Roundy <46587419+wcroundy@users.noreply.github.com>
2019-10-08 20:50:21 -0600

    Create boolchallenge.js

---
+++ basics/boolchallenge.js
@@ -0,0 +1,14 @@
+let age = 75
+
+if(age >= 70)
+{
+    console.log("Yo, you old!")
+}
+else if(age < 7)
+{
+    console.log("He's just a little guy")
+}
+else
+{
+    console.log("No Discount Sucka!")
+}
